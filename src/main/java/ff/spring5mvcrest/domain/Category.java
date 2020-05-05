9f28e557a5ee1eb16567c1bbf945ffe6183af134
federicofrontera <federicofrontera@gmail.com>
2020-05-05 13:09:16 -0300

    fixed annotations

--- src/main/java/ff/spring5mvcrest/domain/Category.java
+++ src/main/java/ff/spring5mvcrest/domain/Category.java
@@ -1,15 +1,19 @@
 package ff.spring5mvcrest.domain;
 
-import org.springframework.data.annotation.Id;
+import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.print.DocFlavor;
 
+@Data
+@Entity
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
 }
