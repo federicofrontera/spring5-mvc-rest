80066cb316774aee46f1feada72535a1f5539b0a
federicofrontera <federicofrontera@gmail.com>
2020-05-07 11:24:55 -0300

    added customer repository

---
+++ src/main/java/ff/spring5mvcrest/repositories/CustomerRepository.java
@@ -0,0 +1,7 @@
+package ff.spring5mvcrest.repositories;
+
+import ff.spring5mvcrest.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
