ab4050ebf37af6367c8d66d9b93a07f91ce1e494
federicofrontera <federicofrontera@gmail.com>
2020-05-07 12:04:23 -0300

    customer controller tests

---
+++ src/test/java/ff/spring5mvcrest/controllers/CustomerControllerTest.java
@@ -0,0 +1,84 @@
+package ff.spring5mvcrest.controllers;
+
+import ff.spring5mvcrest.api.model.CustomerDTO;
+import ff.spring5mvcrest.controllers.v1.CustomerController;
+import ff.spring5mvcrest.services.CustomerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class CustomerControllerTest {
+    @Mock
+    CustomerService customerService;
+
+    @InjectMocks
+    CustomerController customerController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+    }
+
+    @Test
+    public void testListCustomers() throws Exception {
+
+        //given
+        CustomerDTO customer1 = new CustomerDTO();
+        customer1.setFirstName("John");
+        customer1.setLastName("Smith");
+        customer1.setCustomerURL("/api/v1/customer/1");
+
+        CustomerDTO customer2 = new CustomerDTO();
+        customer2.setFirstName("Michael");
+        customer2.setLastName("Policy");
+        customer2.setCustomerURL("/api/v1/customer/2");
+
+        when(customerService.getAllCustomers()).thenReturn(Arrays.asList(customer1, customer2));
+
+        mockMvc.perform(get("/api/v1/customers/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customers", hasSize(2)));
+    }
+
+    @Test
+    public void testGetCustomerById() throws Exception {
+
+        //given
+        CustomerDTO customer1 = new CustomerDTO();
+        customer1.setFirstName("John");
+        customer1.setLastName("Smith");
+        customer1.setCustomerURL("/api/v1/customer/1");
+
+        when(customerService.getCustomerById(anyLong())).thenReturn(customer1);
+
+        //when
+        mockMvc.perform(get("/api/v1/customers/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", equalTo("John")))
+                .andExpect(jsonPath("$.lastName", equalTo("Smith")));
+    }
+}
