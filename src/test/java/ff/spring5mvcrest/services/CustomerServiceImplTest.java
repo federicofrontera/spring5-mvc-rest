85e2a4f1617694c6615312d0ed6ecf1d949a3b95
federicofrontera <federicofrontera@gmail.com>
2020-05-07 11:48:20 -0300

    customer service tests

---
+++ src/test/java/ff/spring5mvcrest/services/CustomerServiceImplTest.java
@@ -0,0 +1,73 @@
+package ff.spring5mvcrest.services;
+
+import ff.spring5mvcrest.api.mapper.CustomerMapper;
+import ff.spring5mvcrest.api.model.CustomerDTO;
+import ff.spring5mvcrest.domain.Customer;
+import ff.spring5mvcrest.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+public class CustomerServiceImplTest {
+
+    @Mock
+    CustomerRepository customerRepository;
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+
+    CustomerService customerService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(Long.valueOf(1));
+        customer1.setFirstName("John");
+        customer1.setLastName("Smith");
+
+        Customer customer2 = new Customer();
+        customer2.setId(Long.valueOf(2));
+        customer2.setFirstName("Michael");
+        customer2.setLastName("Policy");
+
+        when(customerRepository.findAll()).thenReturn(Arrays.asList(customer1, customer2));
+
+        //when
+        List<CustomerDTO> customerDTOS = customerService.getAllCustomers();
+
+        //then
+        assertEquals(2, customerDTOS.size());
+
+    }
+
+    @Test
+    public void getCustomerById() throws Exception {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(Long.valueOf(1));
+        customer1.setFirstName("John");
+        customer1.setLastName("Smith");
+
+        when(customerRepository.findById(anyLong())).thenReturn(java.util.Optional.ofNullable(customer1));
+
+        //when
+        CustomerDTO customerDTO = customerService.getCustomerById(1L);
+
+        assertEquals("John", customerDTO.getFirstName());
+    }
+}
