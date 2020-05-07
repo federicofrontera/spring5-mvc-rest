f4dd1db98d96c33c12f2d2af7004cd5415a8ff13
federicofrontera <federicofrontera@gmail.com>
2020-05-07 11:47:58 -0300

    added customer bootstrap data

--- src/main/java/ff/spring5mvcrest/bootstrap/Bootstrap.java
+++ src/main/java/ff/spring5mvcrest/bootstrap/Bootstrap.java
@@ -1,7 +1,9 @@
 package ff.spring5mvcrest.bootstrap;
 
 import ff.spring5mvcrest.domain.Category;
+import ff.spring5mvcrest.domain.Customer;
 import ff.spring5mvcrest.repositories.CategoryRepository;
+import ff.spring5mvcrest.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +11,20 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        loadCategories();
+        loadCustomers();
+    }
+
+    private void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -37,6 +46,22 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
 
-        System.out.println("Data loaded = " + categoryRepository.count());
+        System.out.println("Categories loaded = " + categoryRepository.count());
+    }
+
+    private void loadCustomers() {
+        Customer customer1 = new Customer();
+        customer1.setId(Long.valueOf(1));
+        customer1.setFirstName("John");
+        customer1.setLastName("Smith");
+        customerRepository.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(Long.valueOf(2));
+        customer2.setFirstName("Michael");
+        customer2.setLastName("Policy");
+        customerRepository.save(customer2);
+
+        System.out.println("Customers loaded = " + customerRepository.count());
     }
 }
