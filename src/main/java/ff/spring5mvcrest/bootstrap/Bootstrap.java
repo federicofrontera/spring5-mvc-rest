b74ad26cec9b3e3a785359f9a9bace9b8e3c90ba
federicofrontera <federicofrontera@gmail.com>
2020-05-08 09:55:58 -0300

    add vendor repository

--- src/main/java/ff/spring5mvcrest/bootstrap/Bootstrap.java
+++ src/main/java/ff/spring5mvcrest/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package ff.spring5mvcrest.bootstrap;
 
 import ff.spring5mvcrest.domain.Category;
 import ff.spring5mvcrest.domain.Customer;
+import ff.spring5mvcrest.domain.Vendor;
 import ff.spring5mvcrest.repositories.CategoryRepository;
 import ff.spring5mvcrest.repositories.CustomerRepository;
+import ff.spring5mvcrest.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,19 @@ public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
     private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         loadCategories();
         loadCustomers();
+        loadVendors();
     }
 
     private void loadCategories() {
@@ -63,5 +68,19 @@ public class Bootstrap implements CommandLineRunner {
         customerRepository.save(customer2);
 
         System.out.println("Customers loaded = " + customerRepository.count());
+    }
+
+    private void loadVendors(){
+        Vendor vendor1 = new Vendor();
+        vendor1.setId(Long.valueOf(1));
+        vendor1.setName("Western Tasy Fruits LTD");
+        vendorRepository.save(vendor1);
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setId(Long.valueOf(2));
+        vendor2.setName("Exotic Fruits Company");
+        vendorRepository.save(vendor2);
+
+        System.out.println("Vendors loaded = " + vendorRepository.count());
     }
 }
